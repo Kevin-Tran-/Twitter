64508abf2390137e79978388e3e070d24b85fb88
Kevin-Tran- <tranpd88@gmail.com>
2016-02-14 00:53:42 -0800

    finished video 1

--- Twitter/AppDelegate.swift
+++ Twitter/AppDelegate.swift
@@ -46,8 +46,23 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         TwitterClient.sharedInstance.fetchAccessTokenWithPath("oauth/access_token", method: "POST", requestToken: BDBOAuth1Credential (queryString: url.query), success: { (accessToken: BDBOAuth1Credential!) -> Void in
             print("Got the access token")
             TwitterClient.sharedInstance.requestSerializer.saveAccessToken(accessToken)
+            
+            TwitterClient.sharedInstance.GET("1.1/account/verify_credentials.json", parameters: nil, success: { (operation: NSURLSessionDataTask, response: AnyObject?) -> Void in
+                print("user: \(response)")
+                }, failure: { (operation: NSURLSessionDataTask?, error: NSError) -> Void in
+                print("error getting current user")
+            })
+            
+            
+            TwitterClient.sharedInstance.GET("1.1/statuses/home_timeline.json", parameters: nil, success: { (operation: NSURLSessionDataTask, response: AnyObject?) -> Void in
+                print("home timeline: \(response)")
+                }, failure: { (operation: NSURLSessionDataTask?, error: NSError) -> Void in
+                    print("error getting home timeline")
+            })
+            
             }) { (error: NSError!) -> Void in
             print("Failed to receive access token \(error)")
+                
         }
         return true
     }
